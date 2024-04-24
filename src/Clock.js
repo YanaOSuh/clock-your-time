035f44f208dc1e72835a654debe76fafda5a11f9
YanaOSuh <yanochkaiv@yahoo.com>
2024-04-24 17:24:46 +0700

    first commit

---
+++ src/Clock.js
@@ -0,0 +1,33 @@
+import { Component } from "react";
+
+export default class Clock extends Component {
+    constructor() {
+    super();
+    this.state = {
+        time: new Date().toLocaleTimeString()
+        }
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() =>
+        this.tick(), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        this.setState({
+            time:new Date().toLocaleTimeString()
+        })
+    }
+
+render() {
+    return(
+    <div>
+        <p className="Time">{this.state.time}</p>
+    </div>
+    )
+}
+}
